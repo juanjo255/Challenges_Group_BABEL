0f6e594006d711416f23376c3cec7d32a824d23a
juanjo255 <piconcossio@gmail.com>
2021-07-15 23:02:35 -0500

    checking problems with mean

--- Reto_sequence_trimming.py
+++ Reto_sequence_trimming.py
@@ -4,8 +4,8 @@ class trim:
         self.fastq = open (fastq).readlines ()
         
     def window (self, window_size, quality) -> None:
-        sec_len = 0
-        last_sec_len = float ("-inf")
+        #sec_len = 0
+        #last_sec_len = float ("-inf")
 
         # parse the document and take quality 
         for i in range (0, len (self.fastq), 4):
@@ -16,36 +16,30 @@ class trim:
             sec_quality = self.fastq [i+3].strip ()
             window_phre_score = list (map (lambda letter: ord (letter)-33 , sec_quality [:window_size]))
             #print (window_phre_score)
-            print (sec_quality)
+            #print (sec_quality)
             if sum (window_phre_score) /window_size < quality:
                 posible_cuts.append ((0, window_size-1))
 
             # we advance in the sec_quiality
-            for ascii in range (4, len (sec_quality)):
+            for ascii in range (5, len (sec_quality)):
                 del window_phre_score [0]
                 window_phre_score.append (ord (sec_quality [ascii])-33)
-                #print (window_phre_score)
+                #print (sec_quality [ascii])
+                print ( sum (window_phre_score) /window_size)
 
-                if sum (window_phre_score) /window_size > 20:
-                    #posible_cuts.append ((ascii-(window_size-1), ascii))
-                    sec_len += 1
-                else:
-                    if last_sec_len < sec_len:
-                        last_sec_len = sec_len
-                        left_right_cut = () 
-                    sec_len = 0
+                #if sum (window_phre_score) /window_size > 20:
+                 #   posible_cuts.append ((ascii-(window_size-1), ascii))
+                    #sec_len += 1
+                #else:
+                 #   if last_sec_len < sec_len:
+                  #      last_sec_len = sec_len
+                        #left_right_cut = () 
+                    #sec_len = 0
 
-            print (id)
-            print (last_sec_len)
-            sec_len = 0
-            break
-            
-"Terminar"
-
+            #print (id)
+            #print (last_sec_len)
+            #sec_len = 0
+            #break
 
 
-
-
-
-trim ("test_sanger.fastq").window (4, 20)
-        
+trim ("test_sanger.fastq").window (5, 20)
